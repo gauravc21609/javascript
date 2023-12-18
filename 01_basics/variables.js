21b2e31df321f5e2a0b75016dca0ac776780f073
gaurav <gchauhan21609@gmail.com>
2023-12-18 15:10:56 +0530

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+const account_Id = 144553;
+
+// account_Id
+
+Let accountEmail = "gaurav486@gmail.com";
+var accountPassword = "gjaura@1";
+
+acountCity = "dehli";
+
+// account_Id = 2   not allowed
+
+accountEmail = "hheglf@gmail.com"
+accountPassword = "klfjld45"
+accountCity ="dehradun"
+
+console.log("account_Id");
+
+console.table([account_Id, accountEmail, accountPassword, accountCity])
