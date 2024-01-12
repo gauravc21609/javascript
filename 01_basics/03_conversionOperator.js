3a212fde667ca39b423a46cb5935b0dd6bb4e10c
gaurav <gchauhan21609@gmail.com>
2024-01-12 16:37:04 +0530

    Arithmatic operator done.

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,62 @@
+let score = "gaurav"
+
+// console.log(typeof score);
+
+// console.log(typeof (score));
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "Saurab"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// " " => false;
+// "saurab" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//  *****************operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(15%9);
+
+let str1 = "hello"
+let str2 = "gaurav"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2 );
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
